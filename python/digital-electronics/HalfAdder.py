40729601121ee2655a3704e0e43d7705376f759a
dshaw0004 <dipankarshaw692@gmail.com>
2024-11-22 19:53:23 +0530

    Half Adder using python made logic gates

---
+++ python/digital-electronics/HalfAdder.py
@@ -0,0 +1,23 @@
+from LogicGates import AND, NOT, OR
+
+
+def Half_Adder(a: int, b: int) -> tuple[int, int]:
+    carry = AND(a, b)
+    SUM: int = OR(
+            AND(
+                a, 
+                NOT(b)
+                ), 
+            AND(
+                NOT(a), 
+                b
+                )
+            )
+    return SUM, carry
+
+
+if __name__ == '__main__':
+    a = int(input('Enter value for a: '))
+    b = int(input('Enter value for b: '))
+    SUM, carry = Half_Adder(a, b)
+    print(f'{SUM = } and {carry = }')
