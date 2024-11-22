b04064c5f509c9a9a7580bd16bdf95784cb13822
dshaw0004 <dipankarshaw692@gmail.com>
2024-11-22 20:04:18 +0530

    Full Adder using python made logic gates

---
+++ python/digital-electronics/FullAdder.py
@@ -0,0 +1,23 @@
+from HalfAdder import Half_Adder
+from LogicGates import AND, NOT, OR
+
+
+def Full_Adder(a: int, b: int, carry_in: int) -> tuple[int, int]:
+    half_adder_sum, half_adder_carry = Half_Adder(a, b)
+    full_adder_sum, _ = Half_Adder(half_adder_sum, carry_in)
+    full_adder_carry = OR(
+            half_adder_carry, 
+            AND(
+                half_adder_sum,
+                carry_in
+                )
+            )
+
+    return full_adder_sum, full_adder_carry
+    
+if __name__ == '__main__':
+    a = int(input('Enter value for a: '))
+    b = int(input('Enter value for b: '))
+    carry_in = int(input('Enter previous sum carry value: '))
+    SUM, carry = Full_Adder(a, b, carry_in)
+    print(f'{SUM = } and {carry = }')
