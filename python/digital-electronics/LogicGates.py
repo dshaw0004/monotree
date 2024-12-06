8ac775656a802f6911abb60043a45fe7872aaf27
dshaw0004 <dipankarshaw692@gmail.com>
2024-12-06 21:59:10 +0530

    XOR and XNOR gate added to logic gates

--- python/digital-electronics/LogicGates.py
+++ python/digital-electronics/LogicGates.py
@@ -1,8 +1,29 @@
-def AND(*args) -> int:
+def AND(*args: int) -> int:
     return int(all([*args]))
 
-def OR(*args) -> int:
+def OR(*args: int) -> int:
     return int(any([*args]))
 
 def NOT(n: int) -> int:
     return int(not n)
+
+def XNOR(a: int, b: int) -> int:
+    return OR(
+            AND(a, b),
+            AND(
+                NOT(a),
+                NOT(b)
+                )
+            )
+
+def XOR(a: int, b: int) -> int:
+    return OR(
+            AND(
+                a,
+                NOT(b)
+                ),
+            AND(
+                NOT(a),
+                b
+                )
+            )
